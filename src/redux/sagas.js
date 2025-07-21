ec39fd3293f3b532e6a60cd84eb688d71b36343a
Olistar <130336147+Olanakelbesa@users.noreply.github.com>
2025-07-21 17:07:34 +0300

    setup Redux store and configure Redux-Saga

---
+++ src/redux/sagas.js
@@ -0,0 +1,54 @@
+import { takeLatest, put, call } from "redux-saga/effects";
+import axios from "axios";
+import { setSongs, addSong, updateSong, deleteSong } from "./songsSlice";
+
+const API_URL = process.env.API_BASE_URL ;
+
+
+function* fetchSongsSaga() {
+    try {
+        const response = yield call(axios.get, API_URL);
+        yield put(setSongs(response.data));
+    } catch (error) {
+        console.error("Fetch error:", error);
+    }
+}
+
+function* addSongSaga(action) {
+    try {
+        const response = yield call(axios.post, API_URL, action.payload, {
+            headers: { "Content-Type": "application/json" }
+        });
+        yield put(addSong(response.data));
+    } catch (error) {
+        console.error("Add song error:", error);
+    }
+}
+
+function* updateSongSaga(action) {
+    try {
+        const { id, data } = action.payload;
+        const response = yield call(axios.put, `${API_URL}/${id}`, data);
+        yield put(updateSong(response.data));
+    } catch (error) {
+        console.error("Update error:", error);
+    }
+}
+
+function* deleteSongSaga(action) {
+    try {
+        yield call(axios.delete, `${API_URL}/${action.payload}`);
+        yield put(deleteSong(action.payload));
+    } catch (error) {
+        console.error("Delete error:", error);
+    }
+}
+
+function* rootSaga() {
+    yield takeLatest("songs/fetchSongs", fetchSongsSaga);
+    yield takeLatest("songs/requestAddSong", addSongSaga); 
+    yield takeLatest("songs/updateSong", updateSongSaga);
+    yield takeLatest("songs/deleteSong", deleteSongSaga);
+}
+
+export default rootSaga;
